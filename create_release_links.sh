101c117c47e28bfa4aeedd5f6f5b332d4e87752c
Melroy van den Berg <melroy@melroy.org>
2019-08-02 14:30:30 +0000

    Automate creating release links

---
+++ create_release_links.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+# By: Melroy van den Berg
+# Description: Create new links in the Release page of Gitlab
+# Depends on two env. variables: $PRIVATE_TOKEN & $APP_VERSION
+
+# Gitlab WineGUI project ID
+project_id=66
+
+# Gitlab URL
+gitlab_url="https://gitlab.melroy.org"
+
+# Website location where you can find the WineGUI binaries
+webpage_prefix="https://winegui.melroy.org/downloads"
+
+if [[ $(curl -s "$gitlab_url/api/v4/projects/$project_id/releases/$APP_VERSION/assets/links?private_token=$PRIVATE_TOKEN") ]]; then
+    echo "INFO: Links already exists. Skipping creating new links for WineGUI $APP_VERSION!"
+else
+    # Upload new links
+    echo "INFO: Creating new release links for WineGUI $APP_VERSION!"
+
+    curl --request POST \
+        --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" \
+        --data name="WineGUI Compressed binary (tar)" \
+        --data url="$webpage_prefix/WineGUI-$APP_VERSION.tar.gz" \
+        "$gitlab_url/api/v4/projects/$project_id/releases/$APP_VERSION/assets/links"
+
+    curl --request POST \
+        --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" \
+        --data name="WineGUI RPM Package (rpm)" \
+        --data url="$webpage_prefix/WineGUI-$APP_VERSION.rpm" \
+        "$gitlab_url/api/v4/projects/$project_id/releases/$APP_VERSION/assets/links"
+
+    curl --request POST \
+        --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" \
+        --data name="WineGUI Debian package (deb)" \
+        --data url="$webpage_prefix/WineGUI-$APP_VERSION.deb" \
+        "$gitlab_url/api/v4/projects/$project_id/releases/$APP_VERSION/assets/links"
+fi
