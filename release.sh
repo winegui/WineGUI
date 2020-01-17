2e87be00547ff01087270424c6a731e784624106
Melroy van den Berg <melroy@melroy.org>
2020-01-17 16:37:31 +0100

    Version check added

--- release.sh
+++ release.sh
@@ -1,7 +1,18 @@
-#!/bin/bash
-# Deploy the tar, deb and rpm files to the WineGUI website download directory
+#!/usr/bin/env bash
+# By: Melroy van den Berg
+# File: release.sh
+# Description: Deploy the tar, deb and rpm files to the WineGUI website download directory
 
-# TODO: Check if the version already exists
+if [ -z ${APP_VERSION} ]; then
+    echo "ERROR: App_version env. variable is not set! Exit"
+    exit 1
+fi
 
-# Roll-out the new release
-sshpass -e scp -o stricthostkeychecking=no ./build/WineGUI-v* melroy@server.melroy.org:/var/www/winegui.melroy.org/html/downloads
+# Check if the version already exists
+output=$(sshpass -e ssh -o StrictHostKeyChecking=no melroy@server.melroy.org 'cd /var/www/winegui.melroy.org/html/downloads; find . -name "WineGUI-${APP_VERSION}.*"')
+if [[ "$output" == "" ]]; then
+    # Roll-out the new release
+    sshpass -e scp -o stricthostkeychecking=no ./build/WineGUI-v* melroy@server.melroy.org:/var/www/winegui.melroy.org/html/downloads
+else
+    echo "INFO: Release is already rolled-out to the downloads folder."
+fi
