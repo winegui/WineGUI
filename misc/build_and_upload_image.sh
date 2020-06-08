dc2ccf1297978ef22e2a41026b79f681563752d7
Melroy van den Berg <melroy@melroy.org>
2020-06-08 03:08:21 +0200

    Creating image based on danger89/cmake

--- misc/build_and_upload_image.sh
+++ misc/build_and_upload_image.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # By: Melroy van den Berg
 # Description: Helper script for creating and uploading new Docker image to Docker Hub
+# Get latest build-on image
 docker build -t gtk3_cmake_ninja .
 # You could also use docker images to look-up the correct image and use the <image_id>
 # Example: docker tag <image_id> danger89/gtk3-docker-cmake-ninja:latest
