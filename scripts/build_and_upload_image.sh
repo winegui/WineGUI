666e2473160cbeceaf68a1359142f53ec98a1483
Melroy van den Berg <melroy@melroy.org>
2021-12-31 06:29:07 +0100

    Fix script

--- scripts/build_and_upload_image.sh
+++ scripts/build_and_upload_image.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 # By: Melroy van den Berg
 # Description: Helper script for creating and uploading new Docker image to Docker Hub
-# Get latest build-on image
-docker build -t gtk3_cmake_ninja ../misc
-# You could also use docker images to look-up the correct image and use the <image_id>
-# Example: docker tag <image_id> danger89/gtk3-docker-cmake-ninja:latest
-docker tag gtk3_cmake_ninja danger89/gtk3-docker-cmake-ninja:latest
+# Create latest image
+docker build -t danger89/gtk3-docker-cmake-ninja ./misc
+# Push
 docker push danger89/gtk3-docker-cmake-ninja
