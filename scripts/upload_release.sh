65e7eac29da4ebc216d6e186eeab22a3352becae
Melroy van den Berg <melroy@melroy.org>
2022-05-02 20:54:09 +0200

    Add extra logging

--- scripts/upload_release.sh
+++ scripts/upload_release.sh
@@ -8,13 +8,18 @@ if [ -z ${APP_VERSION} ]; then
     exit 1
 fi
 
+echo "Listing files in build folder:"
+ls ./build_prod/WineGUI-v*
+
+
 # Check if the version already exists
 output=$(sshpass -e ssh -o StrictHostKeyChecking=no melroy@gitlab.melroy.org 'cd /var/www/winegui.melroy.org/html/downloads; ls')
-
 name="WineGUI-${APP_VERSION}"
 if [[ "$output" == *"$name"* ]]; then
     echo "INFO: Release is already rolled-out to the downloads folder."
 else
+    echo "Transfer files to website..."
+    echo ""
     # Roll-out the new release
     sshpass -e scp -o stricthostkeychecking=no ./build_prod/WineGUI-v* melroy@gitlab.melroy.org:/var/www/winegui.melroy.org/html/downloads
 fi
