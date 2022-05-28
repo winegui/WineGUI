03ea979a2792860ec8186e3f15a29630521ca01e
Melroy van den Berg <melroy@melroy.org>
2022-05-29 00:09:46 +0200

    Fix name

--- scripts/generate_gif.sh
+++ scripts/generate_gif.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Generate gif from screenshots
 # By: Melroy van den Berg
-convert -delay 250 -loop 0 ./misc/winegui_*.png ./misc/screenshots_screenshots.gif
+convert -delay 250 -loop 0 ./misc/winegui_*.png ./misc/winegui_screenshots.gif
