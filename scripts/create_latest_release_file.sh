1f8e5b9854e4f66ad89ace1287c2c6f5465e826e
Melroy van den Berg <melroy@melroy.org>
2022-05-29 16:40:55 +0200

    Create latest_release.txt file

---
+++ scripts/create_latest_release_file.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# By: Melroy van den Berg
+# File: create_latest_release_file.sh
+# Description: Create a latest_release.txt file on winegui.melroy.org,
+#  which can be used to verify if the user is using the latest version of WineGUI.
+
+if [ -z ${APP_VERSION} ]; then
+    echo "ERROR: App_version env. variable is not set! Exit"
+    exit 1
+fi
+
+# Remove the first char ('v') from APP_VERSION
+VERSION="${APP_VERSION:1}"
+# Create text file
+echo "$VERSION" > latest_release.txt
+
+echo "Upload latest_release.txt file..."
+sshpass -e scp -o stricthostkeychecking=no ./latest_release.txt melroy@gitlab.melroy.org:/var/www/winegui.melroy.org/html
