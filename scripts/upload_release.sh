45a9207efa101df340d63a2c9930e82ebd62b893
Melroy van den Berg <melroy@melroy.org>
2022-10-29 23:59:31 +0200

    Use keys to copy files via scp now

--- scripts/upload_release.sh
+++ scripts/upload_release.sh
@@ -10,9 +10,8 @@ fi
 echo "Listing files in build folder:"
 ls ./build_prod/WineGUI-v*
 
-
 # Check if the version already exists
-output=$(sshpass -e ssh -o StrictHostKeyChecking=no melroy@gitlab.melroy.org 'cd /var/www/winegui.melroy.org/html/downloads; ls')
+output=$(ssh -o StrictHostKeyChecking=no melroy@gitlab.melroy.org 'cd /var/www/winegui.melroy.org/html/downloads; ls')
 name="WineGUI-${APP_VERSION}"
 if [[ "$output" == *"$name"* ]]; then
     echo "INFO: Release is already rolled-out to the downloads folder."
@@ -20,5 +19,5 @@ else
     echo "Transfer files to website..."
     echo ""
     # Roll-out the new release
-    sshpass -e scp -o stricthostkeychecking=no ./build_prod/WineGUI-v* melroy@gitlab.melroy.org:/var/www/winegui.melroy.org/html/downloads
+    scp -o stricthostkeychecking=no ./build_prod/WineGUI-v* melroy@gitlab.melroy.org:/var/www/winegui.melroy.org/html/downloads
 fi
