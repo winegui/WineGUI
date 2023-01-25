5aab4c93c8da310d03d43c6d0f49c815050efc66
Melroy van den Berg <melroy@melroy.org>
2023-01-25 22:13:32 +0100

    Create latest release file during the deployment job as well

--- scripts/create_latest_release_file.sh
+++ scripts/create_latest_release_file.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # By: Melroy van den Berg
 # Description: Create a latest_release.txt file on winegui.melroy.org,
-#  which can be used to verify if the user is using the latest version of WineGUI.
+#  which can be used to verify if the user is using the latest version of WineGUI (client-side check).
 
 if [ -z ${APP_VERSION} ]; then
     echo "ERROR: App_version env. variable is not set! Exit"
@@ -10,6 +10,7 @@ fi
 
 # Remove the first char ('v') from APP_VERSION
 VERSION="${APP_VERSION:1}"
-# Create text file
-echo "$VERSION" >latest_release.txt
-
+# Create new folder (release)
+mkdir release
+# Create text file with the latest version
+echo "$VERSION" >release/latest_release.txt
