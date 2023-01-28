d0715aa9dbf0d812a1b59803340d84b24b40f44a
Melroy van den Berg <melroy@melroy.org>
2023-01-28 20:37:05 +0100

    Final commit on create_release_links.sh

--- scripts/create_release_links.sh
+++ scripts/create_release_links.sh
@@ -17,31 +17,29 @@ if [[ "$output" == "" ]]; then
     exit 1
 fi
 
-#if [[ "$output" == "[]" ]]; then
+if [[ "$output" == "[]" ]]; then
+    # Upload new links if the request returns an empty list of links
+    echo "INFO: Creating new release links for WineGUI $APP_VERSION!"
 
-# Upload new links if the request returns an empty list of links
-echo "INFO: Creating new release links for WineGUI $APP_VERSION!"
+    curl --request POST \
+        --header "JOB-TOKEN: $CI_JOB_TOKEN" \
+        --data name="WineGUI Compressed binary (tar)" \
+        --data url="$webpage_prefix/WineGUI-$APP_VERSION.tar.gz" \
+        "https://${CI_SERVER_HOST}/api/v4/projects/${CI_PROJECT_ID}/releases/$APP_VERSION/assets/links"
 
-curl --request POST \
-    --header "JOB-TOKEN: $CI_JOB_TOKEN" \
-    --data name="WineGUI Compressed binary (tar) - test" \
-    --data url="$webpage_prefix/WineGUI-$APP_VERSION-test.tar.gz" \
-    "https://${CI_SERVER_HOST}/api/v4/projects/${CI_PROJECT_ID}/releases/$APP_VERSION/assets/links"
+    curl --request POST \
+        --header "JOB-TOKEN: $CI_JOB_TOKEN" \
+        --data name="WineGUI RPM Package (rpm)" \
+        --data url="$webpage_prefix/WineGUI-$APP_VERSION.rpm" \
+        "https://${CI_SERVER_HOST}/api/v4/projects/${CI_PROJECT_ID}/releases/$APP_VERSION/assets/links"
 
-curl --request POST \
-    --header "JOB-TOKEN: $CI_JOB_TOKEN" \
-    --data name="WineGUI RPM Package (rpm) - test" \
-    --data url="$webpage_prefix/WineGUI-$APP_VERSION-test.rpm" \
-    "https://${CI_SERVER_HOST}/api/v4/projects/${CI_PROJECT_ID}/releases/$APP_VERSION/assets/links"
-
-curl --request POST \
-    --header "JOB-TOKEN: $CI_JOB_TOKEN" \
-    --data name="WineGUI Debian package (deb) - test" \
-    --data url="$webpage_prefix/WineGUI-$APP_VERSION-test.deb" \
-    "https://${CI_SERVER_HOST}/api/v4/projects/${CI_PROJECT_ID}/releases/$APP_VERSION/assets/links"
-
-#elif [[ "$output" == "{\"message\":\"404 Not found\"}" ]]; then
-#    echo "WARN: Release doesn't yet exist yet/can't be found yet in Gitlab: $APP_VERSION..."
-#else
-#    echo "INFO: Links already exists. Skipping creating new links for WineGUI $APP_VERSION!"
-#fi
+    curl --request POST \
+        --header "JOB-TOKEN: $CI_JOB_TOKEN" \
+        --data name="WineGUI Debian package (deb)" \
+        --data url="$webpage_prefix/WineGUI-$APP_VERSION.deb" \
+        "https://${CI_SERVER_HOST}/api/v4/projects/${CI_PROJECT_ID}/releases/$APP_VERSION/assets/links"
+elif [[ "$output" == "{\"message\":\"404 Not found\"}" ]]; then
+    echo "WARN: Release doesn't yet exist yet/can't be found yet in Gitlab: $APP_VERSION..."
+else
+    echo "INFO: Links already exists. Skipping creating new links for WineGUI $APP_VERSION!"
+fi
