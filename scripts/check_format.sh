b0e94f15de2bd8cf2b5057c00ca3f31114d9062d
Melroy van den Berg <melroy@melroy.org>
2023-12-13 00:48:46 +0100

    Support docker check/fix

--- scripts/check_format.sh
+++ scripts/check_format.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-find src/ include/ -iname *.cc -o -iname *.h -o -iname *.h.in | xargs clang-format --dry-run -Werror -style=file -fallback-style=LLVM
+if [ $# -ge 1 ] && [[ $1 == "docker" ]]; then
+  docker run -v $(pwd):/home --rm --workdir /home danger89/gtk3-docker-cmake-ninja:4.0 \
+    bash -c 'find src/ include/ -iname *.cc -o -iname *.h -o -iname *.h.in | xargs clang-format --dry-run -Werror -style=file -fallback-style=LLVM'
+else
+  find src/ include/ -iname *.cc -o -iname *.h -o -iname *.h.in | xargs clang-format --dry-run -Werror -style=file -fallback-style=LLVM -assume-filename=../.clang-format
+fi
