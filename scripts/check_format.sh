84effc3955b8cad8c8ecfd28f46a6db998199520
Melroy van den Berg <melroy@melroy.org>
2023-12-13 01:20:04 +0100

    use the base image instead

--- scripts/check_format.sh
+++ scripts/check_format.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ $# -ge 1 ] && [[ $1 == "docker" ]]; then
-  docker run -v $(pwd):/home --rm --workdir /home danger89/gtk3-docker-cmake-ninja:4.0 \
+  docker run -v $(pwd):/home --rm --workdir /home danger89/cmake:5.0 \
     bash -c 'find src/ include/ -iname *.cc -o -iname *.h -o -iname *.h.in | xargs clang-format --dry-run -Werror -style=file -fallback-style=LLVM'
 else
   find src/ include/ -iname *.cc -o -iname *.h -o -iname *.h.in | xargs clang-format --dry-run -Werror -style=file -fallback-style=LLVM -assume-filename=../.clang-format
