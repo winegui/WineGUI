36c3274b82135f7d1511239ec1dedf1f12cf4c0e
Melroy van den Berg <melroy@melroy.org>
2024-02-11 21:09:26 +0100

    Do not add new line to version.txt

--- scripts/create_source_archive.sh
+++ scripts/create_source_archive.sh
@@ -10,6 +10,6 @@ fi
 # Remove the first char ('v') from APP_VERSION
 VERSION="${APP_VERSION:1}"
 # Create version.txt
-echo "${VERSION}" > version.txt
+echo -n "${VERSION}" > version.txt
 # Create source archive (tar.gz)
 git archive --format=tar.gz --output=build_prod/WineGUI-Source-${APP_VERSION}.tar.gz --add-file=version.txt HEAD
