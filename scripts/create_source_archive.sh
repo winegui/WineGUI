88631367cc1f182124aa4b05d203be963c5b4f81
Melroy van den Berg <melroy@melroy.org>
2024-02-11 20:11:41 +0100

    Remove v from version tag

--- scripts/create_source_archive.sh
+++ scripts/create_source_archive.sh
@@ -7,8 +7,9 @@ if [ -z ${APP_VERSION} ]; then
     echo "ERROR: App_version env. variable is not set! Exit"
     exit 1
 fi
-
+# Remove the first char ('v') from APP_VERSION
+VERSION="${APP_VERSION:1}"
 # Create version.txt
-echo "${APP_VERSION}" > version.txt
+echo "${VERSION}" > version.txt
 # Create source archive (tar.gz)
 git archive --format=tar.gz --output=build_prod/WineGUI-Source-${APP_VERSION}.tar.gz --add-file=version.txt HEAD
