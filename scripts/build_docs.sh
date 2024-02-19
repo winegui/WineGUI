061b5942268271ea5fddeb881ade2bc291441c2a
Melroy van den Berg <melroy@melroy.org>
2024-02-19 02:23:55 +0100

    Move to uppercase options

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # By: Melroy van den Berg
 # Description: Only build the documentation
-cmake -GNinja -Ddoc=ON -B build_docs
+cmake -GNinja -DDOXYGEN=ON -B build_docs
 cmake --build ./build_docs --target Doxygen
--- scripts/build_prod.sh
+++ scripts/build_prod.sh
@@ -3,7 +3,7 @@
 # Description: Release production build + create Debian package file (.deb), 
 #   RPM [Red Hat] Package Manager (.rpm) and compressed file (.tgz/.tar.gz)
 rm -rf build_prod
-cmake -GNinja -DCMAKE_INSTALL_PREFIX:PATH=/usr -Ddoc=ON -Dpackage=ON -DCMAKE_BUILD_TYPE=Release -B build_prod
+cmake -GNinja -DCMAKE_INSTALL_PREFIX:PATH=/usr -DDOXYGEN=ON -DPACKAGE=ON -DCMAKE_BUILD_TYPE=Release -B build_prod
 cmake --build ./build_prod --config Release
 echo "INFO: Building packages..."
 cd build_prod
