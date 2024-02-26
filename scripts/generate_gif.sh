9c97a0384529874f1c625d154c570acec5be0b57
Melroy van den Berg <melroy@melroy.org>
2024-02-26 01:21:04 +0100

    Updated winegui gif

--- scripts/generate_gif.sh
+++ scripts/generate_gif.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Generate gif from screenshots
 # By: Melroy van den Berg
-convert -layers OptimizePlus -delay 250 -loop 0 ./misc/winegui_*.png ./misc/winegui_screenshots.gif
+convert -layers OptimizePlus -delay 250 -loop 0 ./misc/screenshots/*.png ./misc/winegui_screenshots.gif
