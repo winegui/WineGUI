5124673de67c5d3a91f73b0a95603b33ff152841
Melroy van den Berg <melroy@melroy.org>
2024-06-29 21:53:08 +0200

    Fix info or suppress

--- scripts/cpp_check.sh
+++ scripts/cpp_check.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-cppcheck --enable=all,unusedFunction --suppressions-list=suppressions.txt --inline-suppr --error-exitcode=1 -I ./include/ ./src/
+cppcheck --enable=all,unusedFunction --suppressions-list=suppressions.txt --inline-suppr --check-level=exhaustive --error-exitcode=1 -I ./include/ ./src/
