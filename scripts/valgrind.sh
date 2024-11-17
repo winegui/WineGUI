8981d6438b7ebb8d7ac102b1e9fcd2ce952f755f
Melroy van den Berg <melroy@melroy.org>
2024-11-17 01:09:12 +0100

    Also supress gtk leaks from the supp file

--- scripts/valgrind.sh
+++ scripts/valgrind.sh
@@ -6,5 +6,6 @@
 cd ./build/bin
 G_DEBUG=gc-friendly G_SLICE=always-malloc valgrind \
 --suppressions=/usr/share/glib-2.0/valgrind/glib.supp \
+--suppressions=/usr/share/gtk-3.0/valgrind/gtk.supp \
 --leak-check=full --track-origins=yes \
 ./winegui
