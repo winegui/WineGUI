ea470cd18437d60867121743d40a8b6efbdee930
Melroy van den Berg <melroy@melroy.org>
2024-11-17 01:11:58 +0100

    Use the build_debug folder

--- scripts/valgrind.sh
+++ scripts/valgrind.sh
@@ -3,7 +3,7 @@
 # Description: Used for memory leak analysis,
 # be-aware that you will get a lot of false positives messages due to GTK/Glib
 
-cd ./build/bin
+cd ./build_debug/bin
 G_DEBUG=gc-friendly G_SLICE=always-malloc valgrind \
 --suppressions=/usr/share/glib-2.0/valgrind/glib.supp \
 --suppressions=/usr/share/gtk-3.0/valgrind/gtk.supp \
