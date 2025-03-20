ce1115b6b4421a611ac27a91e278744526e89a5d
Melroy van den Berg <melroy@melroy.org>
2025-03-21 00:49:31 +0100

    Cleanup dockerfile

--- scripts/build_and_upload_image.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# By: Melroy van den Berg
-# Description: Helper script for creating and uploading new Docker image to Docker Hub
-# Create latest image
-docker build -t danger89/gtk3-docker-cmake-ninja ./misc
-# Push
-docker push danger89/gtk3-docker-cmake-ninja
