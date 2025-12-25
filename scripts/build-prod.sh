473eaef62174fa1d8c80bd15bd45f35bf5772a1b
Melroy van den Berg <melroy@melroy.org>
2025-12-26 00:10:19 +0100

    Set -e

--- scripts/build-prod.sh
+++ scripts/build-prod.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 # By: Melroy van den Berg
 # Description: Release production build + create Debian package file (.deb), 
-#   RPM [Red Hat] Package Manager (.rpm) and compressed file (.tgz/.tar.gz)
+#  RPM [Red Hat] Package Manager (.rpm) and compressed file (.tgz/.tar.gz)
 #
 # Installs into /usr prefix directory under Linux.
+
+set -e
 
 # Required input parameter check (used for the defining the CPack generators)
 if [ "$1" == "" ]; then
