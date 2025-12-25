8eabab35f800aefa4473501afa2618d5b94e2c78
Melroy van den Berg <melroy@melroy.org>
2025-12-25 22:10:18 +0100

    Just explain that we skip git archive. no exit 1

--- scripts/create-source-archive.sh
+++ scripts/create-source-archive.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 # By: Melroy van den Berg
 # Description: Create custom source code archive with version.txt
-# Depends on one environment variable: $APP_VERSION
+# Depends on one environment variable: $CI_COMMIT_TAG
 
-if [ -z ${APP_VERSION} ]; then
-    echo "ERROR: App_version env. variable is not set! Exit"
-    exit 1
+if [ -z ${CI_COMMIT_TAG} ]; then
+    echo "WARNING: CI_COMMIT_TAG env. variable is not set! WARNING if this should become a new release."
+    echo
+    echo "Skipping git archive command..."
+    exit 0
+else
+    # Remove the first char ('v') from CI_COMMIT_TAG
+    VERSION="${CI_COMMIT_TAG:1}"
+    # Create version.txt
+    echo -n "${VERSION}" > version.txt
+    # Create source archive (tar.gz)
+    git archive --format=tar.gz --output=build_prod/WineGUI-Source-${CI_COMMIT_TAG}.tar.gz --add-file=version.txt HEAD
 fi
-# Remove the first char ('v') from APP_VERSION
-VERSION="${APP_VERSION:1}"
-# Create version.txt
-echo -n "${VERSION}" > version.txt
-# Create source archive (tar.gz)
-git archive --format=tar.gz --output=build_prod/WineGUI-Source-${APP_VERSION}.tar.gz --add-file=version.txt HEAD
