4c70c8ce0ff04a15e727a26b0ff4c397214cef85
Melroy van den Berg <melroy@melroy.org>
2026-01-02 17:20:53 +0100

    Add zenity dep for winetricks

--- cmake/packaging.cmake
+++ cmake/packaging.cmake
@@ -29,17 +29,17 @@ endif ()
 
 if(${LINUX_DISTRO} MATCHES "openSUSE")
   # OpenSuse (Leap, Tumbleweed)
-  set(CPACK_RPM_PACKAGE_REQUIRES "libgtkmm-4_0-0, cabextract, unzip, p7zip, wget")
+  set(CPACK_RPM_PACKAGE_REQUIRES "libgtkmm-4_0-0, cabextract, unzip, p7zip, wget, zenity")
 else()
   # Fedora/CentOS/Redhat/etc.
-  set(CPACK_RPM_PACKAGE_REQUIRES "gtkmm4.0, cabextract, unzip, p7zip, wget")
+  set(CPACK_RPM_PACKAGE_REQUIRES "gtkmm4.0, cabextract, unzip, p7zip, wget, zenity")
 endif()
 # Optional RPM packages
 set(CPACK_RPM_PACKAGE_SUGGESTS "vulkan, vulkan-loader")
 
 # Debian bookworm, trixie, forky, sid, Ubuntu Noble Numbat, Linux Mint 22 (libgtkmm-4.0-0)
 # If needed we can add multiple minor versions eg. via libgtkmm-4.0-0 | libgtkmm-4.0-1
-set(CPACK_DEBIAN_PACKAGE_DEPENDS "libgtkmm-4.0-0, cabextract, unzip, p7zip, wget")
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "libgtkmm-4.0-0, cabextract, unzip, p7zip, wget, zenity")
 # Optional deb packages
 set(CPACK_DEBIAN_PACKAGE_SUGGESTS "libvulkan1, libvulkan1:i386, mesa-vulkan-drivers, mesa-vulkan-drivers:i386")
 
